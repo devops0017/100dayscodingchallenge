bb93b118d3dfff462aa5f68cdfc41df7ccc4b0b5
devops0017 <devops0017@gmail.com>
2021-08-21 23:43:19 +0530

    decimal to roman conversion exercise

---
+++ 21_Aug_2021.py
@@ -0,0 +1,25 @@
+class RomanNumerals:
+
+    def logic(self,number):
+        num_len = len(number)
+        roman_num = ''
+        if len(number) == 4
+            roman_num = str(number).split()[0] * 'M'
+            num_len -= num_len
+            if number[num_len-1] < 490:
+                roman_num = roman_num + str(number)[1] * 'C'
+            elif number[num_len-1] > 490 and number[num_len-1] < 990:
+                roman_num = roman_num + 'D'
+            elif number[num_len-1] > 990:
+                roman_num = roman_num + 'XM'
+
+
+
+
+
+
+
+    def to_roman(self,number):
+        split_number = str(number).split()
+        number = [0]+(len(split_number)-1)
+
