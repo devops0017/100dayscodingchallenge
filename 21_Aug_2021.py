d4dbe0938f4e777c6977979d994c2deb81200ec1
devops0017 <devops0017@gmail.com>
2021-08-22 23:36:01 +0530

    No progess

--- 21_Aug_2021.py
+++ 21_Aug_2021.py
@@ -3,13 +3,14 @@ class RomanNumerals:
     def logic(self,number):
         num_len = len(number)
         roman_num = ''
-        if len(number) == 4
+        if len(number) == 4:
             roman_num = str(number).split()[0] * 'M'
             num_len -= num_len
-            if number[num_len-1] < 490:
-                roman_num = roman_num + str(number)[1] * 'C'
-            elif number[num_len-1] > 490 and number[num_len-1] < 990:
+            if number[num_len-1] > 490 and number[num_len-1] < 990:
                 roman_num = roman_num + 'D'
+            elif number[num_len-1] < 490:
+                roman_num = roman_num + str(number)[1] * 'C'
+
             elif number[num_len-1] > 990:
                 roman_num = roman_num + 'XM'
 
