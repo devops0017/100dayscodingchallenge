e2cd107ff4936fa9909ba3c8ac36d2e30bc0cd89
devops0017 <devops0017@gmail.com>
2021-08-24 20:55:37 +0530

    employee_mgmt

--- employee_mgmt.py
+++ employee_mgmt.py
@@ -15,13 +15,17 @@ cursor = conn.cursor()
 print('********* Employee Management System *************')
 print('Select action\n1. Add employee\n2. Find employee')
 inp = input('Enter choice: ')
-if inp == 1:
+if inp == '1':
     emp_first = input('Enter first name: ')
     emp_last = input('Enter Last name: ')
     emp_salary = input('Enter Salary: ')
     emp = Employee(emp_first,emp_last,emp_salary,cursor)
-    emp.add_employee
-elif inp == 2:
-    emp = Employee(emp_first,emp_last,emp_salary,cursor)
-
-print('\nRecord has been successfully added!!!')
+    emp.add_employee(conn)
+    print('\nRecord has been successfully added!!!')
+elif inp == '2':
+    next_srch = 'y'
+    while next_srch == 'y':
+        name = input('Enter employee name: ')
+        emp = Employee.get_employee(cursor,name)
+        print(emp)
+        next_srch = input('Do you want to search another employee (y/n): ')
--- schema.py
+++ schema.py
@@ -1,4 +1,5 @@
 class Employee:
+
     def __init__(self,first_name,last_name,salary,sql):
         self.first_name = first_name
         self.last_name = last_name
@@ -10,9 +11,13 @@ class Employee:
             self.sql.execute("INSERT INTO employees VALUES (:first, :last, :salary)",
                          {'first': self.first_name, 'last': self.last_name, 'salary': self.salary})
 
-    def get_employee(self):
-        self.sql.execute("SELECT * FROM employees")
-        return self.sql.fetchall()
+    def get_employee(c,name):
+        c.execute("SELECT * FROM employees WHERE first='{}'".format(name))
+        return c.fetchall()
+    #
+    # def get_employee(c):
+    #     c.execute("SELECT * FROM employees")
+    #     return c.fetchall()
 
     def del_employee(self):
         pass
